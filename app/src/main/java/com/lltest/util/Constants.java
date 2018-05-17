661064527f3452c5f9b3bd6829ebddae57a35d4d
Louis Lu <louis.lu212@gmail.com>
2018-05-16 22:14:26 -0700

    merge test: change from lenovo laptop

--- app/src/main/java/com/lltest/util/Constants.java
+++ app/src/main/java/com/lltest/util/Constants.java
@@ -15,6 +15,8 @@ public class Constants {
     public static final int SWIPE_MIN_DISTANCE = 120;
     public static final int SWIPE_MAX_OFF_PATH = 250;
     public static final int SWIPE_THRESHOLD_VELOCITY = 200;
+    
+    // Test: change from lenovo laptop
 
 
     // fragment tags:
