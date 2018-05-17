792755432788a31d30bbbee66d75459eed80f172
Louis Lu <louis.lu212@gmail.com>
2018-05-16 22:26:21 -0700

    merge test: from harman

--- app/src/main/java/com/lltest/util/Constants.java
+++ app/src/main/java/com/lltest/util/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
 
     public static final int SWIPE_MIN_DISTANCE = 120;
     public static final int SWIPE_MAX_OFF_PATH = 250;
-    public static final int SWIPE_THRESHOLD_VELOCITY = 100;
+    public static final int SWIPE_THRESHOLD_VELOCITY = 200;
     
     // Test: change from lenovo laptop
 
